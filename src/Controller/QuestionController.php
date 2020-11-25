da96649ed432d628b5e9abd79e40791efcaf9813
Patrick <Patrick93@hotmail.de>
2020-11-25 16:49:18 +0100

    KnpMarkdownBundle & Service

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -1,12 +1,10 @@
 <?php
-
 namespace App\Controller;
-
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
-
 class QuestionController extends AbstractController
 {
     /**
@@ -17,26 +15,26 @@ class QuestionController extends AbstractController
         /*
         // fun example of using the Twig service directly!
         $html = $twigEnvironment->render('question/homepage.html.twig');
-
         return new Response($html);
         */
-
         return $this->render('question/homepage.html.twig');
     }
-
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug)
+    public function show($slug, MarkdownParserInterface $markdownParser)
     {
         $answers = [
-            'Make sure your cat is sitting purrrfectly still 🤣',
+            'Make sure your cat is sitting `purrrfectly` still 🤣',
             'Honestly, I like furry shoes better than MY cat',
             'Maybe... try saying the spell backwards?',
         ];
+        $questionText = 'I\'ve been turned into a cat, any thoughts on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
+        $parsedQuestionText = $markdownParser->transformMarkdown($questionText);
 
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
+            'questionText' => $parsedQuestionText,
             'answers' => $answers,
         ]);
     }
